a43d7018010ff74ba8effe7202095675406686ba
Tom French <tom@tomfren.ch>
2023-05-19 11:27:17 +0100

    chore: remove transpilation step before browser tests

---
+++ web-test-runner.config.mjs
@@ -0,0 +1,10 @@
+import { esbuildPlugin } from "@web/dev-server-esbuild";
+
+export default {
+  files: ["src/**/*.test.ts", "src/**/*.spec.ts"],
+  plugins: [
+    esbuildPlugin({
+      ts: true,
+    }),
+  ],
+};
