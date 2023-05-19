10166de89bfa1bc692ec3d70b0d50fb9678972aa
Tom French <tom@tomfren.ch>
2023-05-19 10:55:16 +0100

    fix: update import path

--- test/browser/abi_encode.test.ts
+++ test/browser/abi_encode.test.ts
@@ -1,5 +1,5 @@
 import { html, fixture, expect } from "@open-wc/testing";
-import "../../my-element.js";
+import "./boilerplate/my_element";
 
 describe("noir wasm compilation", () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
