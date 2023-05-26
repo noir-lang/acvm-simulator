98ef4764c0e5b6064f75429b465e3c9240b86ea4
jonybur <jobur93@gmail.com>
2023-05-26 11:59:39 +0000

    Append version from Cargo.toml to package.json

--- installPhase.sh
+++ installPhase.sh
@@ -15,11 +15,19 @@ else
     VERSION_APPENDIX="-NOGIT"
 fi
 
+# Extract version from Cargo.toml
+PACKAGE_VERSION=$(grep '^version =' Cargo.toml | sed -E 's/version = "(.*)"$/\1/')
+if [ -z "$PACKAGE_VERSION" ]; then
+    echo "Could not extract version from Cargo.toml"
+    exit 1
+fi
+PACKAGE_VERSION+=$VERSION_APPENDIX
+
 mkdir -p $out
 cp README.md $out/
 cp -r ./pkg/* $out/
 cat package.json \
-| jq '{ name, version, collaborators, license } 
+| jq --arg ver "$PACKAGE_VERSION" '{ name, version: $ver, collaborators, license } 
 | .repository = {"type": "git","url": "https://github.com/noir-lang/acvm-simulator-wasm.git"} 
 | .sideEffects = false | .files = ["nodejs","web","package.json"] 
 | .main = "./nodejs/acvm_simulator.js" 
