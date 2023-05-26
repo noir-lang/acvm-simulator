d5a590ca943b8e16dcfbdf155b087000009d4a76
Koby <kobyhall@proton.me>
2023-05-26 11:42:26 +0200

    fix: debug info

--- flake.nix
+++ flake.nix
@@ -142,15 +142,14 @@
         buildPhaseCargoCommand = ''
           cargo build --lib --release --target wasm32-unknown-unknown
           wasm-bindgen ./target/wasm32-unknown-unknown/release/acvm_simulator.wasm --out-dir ./pkg/nodejs --typescript --target nodejs
-          wasm-bindgen ./target/wasm32-unknown-unknown/release/acvm_simulator.wasm --out-dir ./pkg/browser --typescript --target web
+          wasm-bindgen ./target/wasm32-unknown-unknown/release/acvm_simulator.wasm --out-dir ./pkg/web --typescript --target web
           wasm-opt ./pkg/nodejs/acvm_simulator_bg.wasm -o ./pkg/nodejs/acvm_simulator_bg.wasm -O
           wasm-opt ./pkg/web/acvm_simulator_bg.wasm -o ./pkg/web/acvm_simulator_bg.wasm -O
-          ls ./pkg/ -R -lah
         '';
 
         installPhase = ''
           mkdir -p $out
-          cp README.md $out
+          cp README.md $out/
           cp -r ./pkg/* $out/
           cat package.json \
           | jq '{ name, version, collaborators, license } 
