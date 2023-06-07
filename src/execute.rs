bd7576121d487eaf4442d85172457da074d5f0bb
Tom French <tom@tomfren.ch>
2023-06-07 14:16:30 +0100

    chore: remove unwanted underscores

--- src/execute.rs
+++ src/execute.rs
@@ -30,17 +30,17 @@ struct SimulatedBackend {
 impl PartialWitnessGenerator for SimulatedBackend {
     fn schnorr_verify(
         &self,
-        _initial_witness: &mut WitnessMap,
-        _public_key_x: &FunctionInput,
-        _public_key_y: &FunctionInput,
-        _signature: &[FunctionInput],
-        _message: &[FunctionInput],
-        _output: &Witness,
+        initial_witness: &mut WitnessMap,
+        public_key_x: &FunctionInput,
+        public_key_y: &FunctionInput,
+        signature: &[FunctionInput],
+        message: &[FunctionInput],
+        output: &Witness,
     ) -> Result<OpcodeResolution, OpcodeResolutionError> {
         // In barretenberg, if the signature fails, then the whole thing fails.
 
-        let pub_key_x = witness_to_value(_initial_witness, _public_key_x.witness)?.to_be_bytes();
-        let pub_key_y = witness_to_value(_initial_witness, _public_key_y.witness)?.to_be_bytes();
+        let pub_key_x = witness_to_value(initial_witness, public_key_x.witness)?.to_be_bytes();
+        let pub_key_y = witness_to_value(initial_witness, public_key_y.witness)?.to_be_bytes();
 
         let pub_key_bytes: Vec<u8> = pub_key_x.iter().copied().chain(pub_key_y.to_vec()).collect();
         let pub_key: [u8; 64] = pub_key_bytes.try_into().map_err(|v: Vec<u8>| {
@@ -50,10 +50,10 @@ impl PartialWitnessGenerator for SimulatedBackend {
             )
         })?;
 
-        let signature_bytes: Vec<u8> = _signature
+        let signature_bytes: Vec<u8> = signature
             .iter()
             .map(|sig_elem| {
-                let witness_value = *witness_to_value(_initial_witness, sig_elem.witness)?;
+                let witness_value = *witness_to_value(initial_witness, sig_elem.witness)?;
                 witness_value.to_be_bytes().last().expect("byte array is never empty")
             })
             .collect::<Result<_, _>>()?;
@@ -61,67 +61,67 @@ impl PartialWitnessGenerator for SimulatedBackend {
         let sig_s = signature_bytes[0..32].try_into().map_err(|_| {
             OpcodeResolutionError::BlackBoxFunctionFailed(
                 BlackBoxFunc::SchnorrVerify,
-                format!("signature should be 64 bytes long, found only {} bytes", _signature.len()),
+                format!("signature should be 64 bytes long, found only {} bytes", signature.len()),
             )
         })?;
         let sig_e = signature_bytes[32..64].try_into().map_err(|_| {
             OpcodeResolutionError::BlackBoxFunctionFailed(
                 BlackBoxFunc::SchnorrVerify,
-                format!("signature should be 64 bytes long, found only {} bytes", _signature.len()),
+                format!("signature should be 64 bytes long, found only {} bytes", signature.len()),
             )
         })?;
 
-        let message_bytes: Vec<u8> = _message
+        let message_bytes: Vec<u8> = message
             .iter()
             .map(|message_elem| {
-                let witness_value = *witness_to_value(_initial_witness, message_elem.witness)?;
+                let witness_value = *witness_to_value(initial_witness, message_elem.witness)?;
                 witness_value.to_be_bytes().last().expect("byte array is never empty")
             })
             .collect::<Result<_, _>>()?;
 
-        let valid_signature = self
+        let validsignature = self
             .blackbox_vendor
-            .verify_signature(pub_key, sig_s, sig_e, &message_bytes)
+            .verifysignature(pub_key, sig_s, sig_e, &message_bytes)
             .map_err(|err| {
                 OpcodeResolutionError::BlackBoxFunctionFailed(
                     BlackBoxFunc::SchnorrVerify,
                     err.to_string(),
                 )
             })?;
-        if !valid_signature {
+        if !validsignature {
             dbg!("signature has failed to verify");
         }
 
-        _initial_witness.insert(*_output, FieldElement::from(valid_signature));
+        initial_witness.insert(*output, FieldElement::from(validsignature));
         Ok(OpcodeResolution::Solved)
     }
 
     fn pedersen(
         &self,
-        _initial_witness: &mut WitnessMap,
-        _inputs: &[FunctionInput],
-        _domain_separator: u32,
-        _outputs: &[Witness],
+        initial_witness: &mut WitnessMap,
+        inputs: &[FunctionInput],
+        domain_separator: u32,
+        outputs: &[Witness],
     ) -> Result<OpcodeResolution, OpcodeResolutionError> {
         let scalars: Result<Vec<_>, _> =
-            _inputs.iter().map(|input| witness_to_value(_initial_witness, input.witness)).collect();
+            inputs.iter().map(|input| witness_to_value(initial_witness, input.witness)).collect();
         let scalars: Vec<_> = scalars?.into_iter().cloned().collect();
 
         let (res_x, res_y) = self.blackbox_vendor.encrypt(scalars).map_err(|err| {
             OpcodeResolutionError::BlackBoxFunctionFailed(BlackBoxFunc::Pedersen, err.to_string())
         })?;
-        _initial_witness.insert(_outputs[0], res_x);
-        _initial_witness.insert(_outputs[1], res_y);
+        initial_witness.insert(outputs[0], res_x);
+        initial_witness.insert(outputs[1], res_y);
         Ok(OpcodeResolution::Solved)
     }
 
     fn fixed_base_scalar_mul(
         &self,
-        _initial_witness: &mut WitnessMap,
-        _input: &FunctionInput,
-        _outputs: &[Witness],
+        initial_witness: &mut WitnessMap,
+        input: &FunctionInput,
+        outputs: &[Witness],
     ) -> Result<OpcodeResolution, OpcodeResolutionError> {
-        let scalar = witness_to_value(_initial_witness, _input.witness)?;
+        let scalar = witness_to_value(initial_witness, input.witness)?;
 
         let (pub_x, pub_y) = self.blackbox_vendor.fixed_base(scalar).map_err(|err| {
             OpcodeResolutionError::BlackBoxFunctionFailed(
@@ -130,8 +130,8 @@ impl PartialWitnessGenerator for SimulatedBackend {
             )
         })?;
 
-        _initial_witness.insert(_outputs[0], pub_x);
-        _initial_witness.insert(_outputs[1], pub_y);
+        initial_witness.insert(outputs[0], pub_x);
+        initial_witness.insert(outputs[1], pub_y);
         Ok(OpcodeResolution::Solved)
     }
 }
