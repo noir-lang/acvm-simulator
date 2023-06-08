49345b737bf5ad9215843f26ec523136b4cf668c
Tom French <tom@tomfren.ch>
2023-06-08 13:49:10 +0100

    chore: run tests on all 3 main browsers

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -2,7 +2,11 @@ import { esbuildPlugin } from "@web/dev-server-esbuild";
 import { playwrightLauncher } from "@web/test-runner-playwright";
 
 export default {
-  browsers: [playwrightLauncher({ product: "webkit" })],
+  browsers: [
+    playwrightLauncher({ product: "chromium" }),
+    playwrightLauncher({ product: "firefox" }),
+    playwrightLauncher({ product: "webkit" }),
+  ],
   plugins: [
     esbuildPlugin({
       ts: true,
