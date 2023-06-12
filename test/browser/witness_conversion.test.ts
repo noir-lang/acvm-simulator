02609721b4dd1f1605e6667ee07682d666b831a3
TomAFrench <tom@tomfren.ch>
2023-06-12 16:03:09 +0000

    chore: fix browser test

--- test/browser/witness_conversion.test.ts
+++ test/browser/witness_conversion.test.ts
@@ -1,17 +1,21 @@
 import { expect } from "@esm-bundle/chai";
-import { compressWitness, decompressWitness } from "../../result/";
+import initACVMSimulator, { compressWitness, decompressWitness } from "../../result/";
 import {
   expectedCompressedWitnessMap,
   expectedWitnessMap,
 } from "../shared/witness_compression";
 
-it("successfully compresses the witness", () => {
+it("successfully compresses the witness", async () => {
+  await initACVMSimulator();
+
   const compressedWitnessMap = compressWitness(expectedWitnessMap);
 
   expect(compressedWitnessMap).to.be.deep.eq(expectedCompressedWitnessMap);
 });
 
-it("successfully decompresses the witness", () => {
+it("successfully decompresses the witness", async () => {
+  await initACVMSimulator();
+
   const witnessMap = decompressWitness(expectedCompressedWitnessMap);
 
   expect(witnessMap).to.be.deep.eq(expectedWitnessMap);
