ffbceaaca390a3781cd5f695aca0c424cf3ee378
TomAFrench <tom@tomfren.ch>
2023-06-20 11:09:52 +0000

    chore: spelling

--- src/build_info.rs
+++ src/build_info.rs
@@ -8,7 +8,7 @@ const LOG_LEVEL: &'static str = r#"
 * @typedef {Object} BuildInfo - Information about how the installed package was built
 * @property {string} gitHash - The hash of the git commit from which the package was built. 
 * @property {string} version - The version of the package at the built git commit.
-* @property {string} dirty - Whether the package contained uncommited changes when built.
+* @property {string} dirty - Whether the package contained uncommitted changes when built.
  */
 export type BuildInfo = {
   gitHash: string;
