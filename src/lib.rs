b2a79ed6d353a2f37a102fda2a3045e0d7199515
TomAFrench <tom@tomfren.ch>
2023-06-20 10:39:46 +0000

    chore: move build info into its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 #![warn(unused_crate_dependencies, unused_extern_crates)]
 #![warn(unreachable_pub)]
 
-use gloo_utils::format::JsValueSerdeExt;
-use serde::{Deserialize, Serialize};
-use wasm_bindgen::prelude::*;
-
 mod abi;
 mod barretenberg;
+mod build_info;
 mod compression;
 mod execute;
 mod foreign_calls;
@@ -15,27 +12,9 @@ mod logging;
 mod public_witness;
 
 pub use abi::{abi_decode, abi_encode};
+pub use build_info::build_info;
 pub use compression::{compress_witness, decompress_witness};
 pub use execute::execute_circuit;
 pub use js_witness_map::JsWitnessMap;
 pub use logging::{init_log_level, LogLevel};
 pub use public_witness::{get_public_parameters_witness, get_public_witness, get_return_witness};
-
-#[derive(Serialize, Deserialize)]
-pub struct BuildInfo {
-    git_hash: &'static str,
-    version: &'static str,
-    dirty: &'static str,
-}
-
-const BUILD_INFO: BuildInfo = BuildInfo {
-    git_hash: env!("GIT_COMMIT"),
-    version: env!("CARGO_PKG_VERSION"),
-    dirty: env!("GIT_DIRTY"),
-};
-
-#[wasm_bindgen(js_name = buildInfo)]
-pub fn build_info() -> JsValue {
-    console_error_panic_hook::set_once();
-    <JsValue as JsValueSerdeExt>::from_serde(&BUILD_INFO).unwrap()
-}
